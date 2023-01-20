697b31cb5195ebf221683104333325762d7330de
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-01-20 16:52:41 +0100

    ADD : Check if a user exists in the database with his credentials

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
 app.use(express.static('views'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
@@ -7,4 +9,31 @@ app.get('/', function(req, res) {
 
 app.listen(3000, function() {
     console.log('Server started on port 3000');
+});
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'openai'
+    });
+
+     connection.query(`SELECT * FROM user WHERE mail = '${email}' AND password = '${password}'`,
+         function(err, results, fields) {
+                console.log(results);
+             if (results.length > 0) {
+                 console.log('User found');
+             } else {
+                 console.log('User not found');
+                 res.sendStatus(401);             }
+        });
+    res.sendFile(__dirname + '/views/index.html');
+
+
 });
