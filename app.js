6c839e08ae789c02a3b240cb8545fa54a6edaff6
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-01-26 15:37:31 +0100

    ADD: Error message when the user types an incorrect login info

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 const mysql = require('mysql2');
+const { Server } = require("socket.io");
+const io = new Server(server);
+const flash = require('express-flash');
 
 app.use(express.static('views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-app.use(require("express-session")({
+app.use('/css', express.static('node_modules/bootstrap/dist/css'))
+app.use('/js', express.static('node_modules/bootstrap/dist/js'))
+
+app.use(flash());
+
+const sessionMiddleware = session({
     secret: "test",
     resave: false,
     saveUninitialized: false
-}));
+});
+
+app.use(sessionMiddleware);
 
 app.get('/', function(req, res){
     if (req.session.loggedin) {
         res.redirect('/app');
     }
     else {
-        res.render(__dirname + '/views/login_page.html');
+        res.render(__dirname + '/views/login_page.ejs');
     }
 });
 
@@ -30,7 +43,7 @@ app.get('/app', function(req, res){
     }
 });
 
-app.listen(3000, function() {
+server.listen(3000, function() {
     console.log('Server started on port 3000');
 });
 
@@ -57,7 +70,11 @@ app.post('/login', (req, res) => {
                  res.redirect('/app');
 
              } else {
-                 res.sendStatus(401);
+                 req.flash('info', 'Identifiants incorrects');
+                 res.redirect('/');
              }
         });
+});
+
+io.on('connection', (socket) => {
 });
