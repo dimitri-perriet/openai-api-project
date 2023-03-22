0cca407ffb9471c3ed2cf5e5ce4caa6c2e58f389
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-03-22 16:34:21 +0100

    EDIT : Change to database architecture to respect the MLD diagrams

--- openai.sql
+++ openai.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 27 jan. 2023 à 15:47
+-- Généré le : mer. 22 mars 2023 à 15:33
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+01:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -24,25 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE IF NOT EXISTS `chat` (
+    `ID` int(11) NOT NULL,
+    `user_id` int(11) DEFAULT NULL,
+    `created` datetime DEFAULT NULL,
+    `updated` datetime DEFAULT NULL,
+    PRIMARY KEY (`ID`),
+    KEY `user_id` (`user_id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conversation`
+--
+
+DROP TABLE IF EXISTS `conversation`;
+CREATE TABLE IF NOT EXISTS `conversation` (
+    `ID` int(11) NOT NULL,
+    `chat_id` int(11) DEFAULT NULL,
+    `message` longtext,
+    `created` datetime DEFAULT NULL,
+    PRIMARY KEY (`ID`),
+    KEY `chat_id` (`chat_id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `games`
 --
 
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE IF NOT EXISTS `games` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `games`
---
-
-INSERT INTO `games` (`ID`, `user_id`, `name`) VALUES
-(6, 1, 'The Last of Us'),
-(4, 1, 'Modern Warfare'),
-(7, 1, 'Dead By Daylight');
+    `ID` int(11) NOT NULL,
+    `user_id` int(11) DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `created` datetime DEFAULT NULL,
+    `updated` datetime DEFAULT NULL,
+    PRIMARY KEY (`ID`),
+    KEY `user_id` (`user_id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -52,19 +78,15 @@ INSERT INTO `games` (`ID`, `user_id`, `name`) VALUES
 
 DROP TABLE IF EXISTS `game_character`;
 CREATE TABLE IF NOT EXISTS `game_character` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `game` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `details` longtext,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `game_character`
---
-
-INSERT INTO `game_character` (`ID`, `game`, `name`, `details`) VALUES
-(10, 4, 'Mason', '1. Nom: Mason\n2. Apparence: Un homme de taille moyenne avec des cheveux noirs et des yeux bleus\n3. Personnalité: Déterminé, courageux et loyal\n4. Compétences: Excellente maîtrise des armes à feu et des arts martiaux\n5. Histoire: Un ancien soldat qui a été recruté pour une mission secrète et qui a été forcé de se battre pour sa survie et celle de ses compagnons');
+    `ID` int(11) NOT NULL,
+    `game` int(11) DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `details` longtext,
+    `created` datetime DEFAULT NULL,
+    `updated` datetime DEFAULT NULL,
+    PRIMARY KEY (`ID`),
+    KEY `game` (`game`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -74,20 +96,15 @@ INSERT INTO `game_character` (`ID`, `game`, `name`, `details`) VALUES
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `mail` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`ID`, `mail`, `password`, `lastname`, `firstname`) VALUES
-(1, 'test@test.fr', '1234', 'Di', 'Pe');
+    `ID` int(11) NOT NULL,
+    `mail` varchar(255) DEFAULT NULL,
+    `password` varchar(255) DEFAULT NULL,
+    `lastname` varchar(255) DEFAULT NULL,
+    `firstname` varchar(255) DEFAULT NULL,
+    `created` datetime DEFAULT NULL,
+    `updated` datetime DEFAULT NULL,
+    PRIMARY KEY (`ID`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
