b4c3bfb45dd7e16b5565a97b55e28b18dea48c10
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-03-29 11:43:35 +0200

    FIX : Dynamic header not working & crash

--- views/src/App.js
+++ views/src/App.js
@@ -8,18 +8,15 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Footer from "./components/Footer";
 import ProtectedRoutes from "./components/ProtectedRoutes";
-import Header from "./components/Header";
 
 function App() {
-    let user = JSON.parse(sessionStorage.getItem('user'));
-    return (
+  return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout /> }>
             <Route index element={<Home />} />
             <Route path="dashboard" element={
                 <ProtectedRoutes>
-                    <Header title={"Que souhaites-tu faire, " + user.firstname + " ?"}/>
                     <Dashboard/>
                     <Footer/>
                 </ProtectedRoutes>
--- views/src/pages/Dashboard.jsx
+++ views/src/pages/Dashboard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import DashboardCard from "../components/DashboardCard";
+import Header from "../components/Header";
 
 function Dashboard() {
+    let user = JSON.parse(sessionStorage.getItem("user"));
     return (
-        <div className="grid grid-cols-3 auto-cols-max justify-items-center mt-24">
-            <DashboardCard justifyclass="justify-self-end" image="./icon/add-circle.svg" text="Ajouter un jeu"/>
-            <DashboardCard justifyclass="" image="./icon/person-add.svg" text="Créer un personnage"/>
-            <DashboardCard justifyclass="justify-self-start"image="./icon/game-controller.svg" text="Accèdes à tes jeux"/>
+        <div>
+            <Header title={"Que souhaites-tu faire, " + user.firstname +" ?"}/>
+            <div className="grid grid-cols-3 auto-cols-max justify-items-center mt-24">
+                <DashboardCard justifyclass="justify-self-end" image="./icon/add-circle.svg" text="Ajouter un jeu"/>
+                <DashboardCard justifyclass="" image="./icon/person-add.svg" text="Créer un personnage"/>
+                <DashboardCard justifyclass="justify-self-start" image="./icon/game-controller.svg" text="Accèdes à tes jeux"/>
+            </div>
         </div>
     )
 }
