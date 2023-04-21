3661e1081348783ec8c0581e5237fadad468cc7d
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-04-21 15:04:13 +0200

    ADD : Game library in Character page

--- views/src/pages/Character.jsx
+++ views/src/pages/Character.jsx
@@ -1,42 +1,56 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "../components/Header";
 import {decodeToken} from "react-jwt";
-import { register } from 'swiper/element/bundle';
+import {register} from 'swiper/element/bundle';
 
 
-function Character ()
-{
+function Character() {
     register();
 
     const token = JSON.parse(sessionStorage.getItem('token'));
     const user = decodeToken(token);
     let [game, setGame] = React.useState(null);
+    let [currentGame, setCurrentGame] = React.useState(null);
 
-    function handleEmptyImg()
-    {
+
+
+
+    let myHeaders = new Headers();
+    myHeaders.append("Bearer", token);
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+    };
+
+    fetch("api/games/user/" + user.id, requestOptions)
+        .then(response => response.json())
+        .then(data => {setGame(data)})
+        .catch(error => console.log('error', error));
+
+    function handleEmptyImg() {
         const swiperEl = document.querySelector('swiper-container');
-            swiperEl.swiper.slideNext();
-            console.log(swiperEl.swiper.activeIndexChange)
-
+        setCurrentGame(game[swiperEl.swiper.activeIndex])
     }
+
+    useEffect(() => {
+        console.log(currentGame)
+    }, [currentGame]);
 
     return (
         <div>
             <Header title={"Top ! Ajoutons un personnage, " + user.firstname + " !"}/>
 
-            { !game &&
+            {game &&
                 <div>
-                <swiper-container class="mySwiper" effect="cards" grab-cursor="true">
-                    <swiper-slide>Slide 1</swiper-slide>
-                    <swiper-slide>Slide 2</swiper-slide>
-                    <swiper-slide>Slide 3</swiper-slide>
-                </swiper-container>
+                    <swiper-container class="mySwiper" effect="cards" grab-cursor="true">
+                        { game.map(singleGame => <swiper-slide> <img src={singleGame.cover} alt={singleGame.name}/> </swiper-slide>)}
+                    </swiper-container>
 
-                <button className={"mx-auto"} onClick={handleEmptyImg}>Ok</button>
+                    <button className={"mx-auto"} onClick={handleEmptyImg}>Ok</button>
 
-                    </div>
+                </div>
             }
-
 
         </div>
     )
