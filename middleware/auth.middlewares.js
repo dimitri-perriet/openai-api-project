e09cefd0661e4e5af921bd46a371a0ad5449e783
Dimitri <97532296+dimitri-perriet@users.noreply.github.com>
2023-06-07 16:08:26 +0200

    Edit : Change Bearer Header to Authorization

--- middleware/auth.middlewares.js
+++ middleware/auth.middlewares.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
 import config from "../config/config.js";
 export const auth = (req, res, next) => {
-    const token = req.header('Bearer');
-    if (!token) {
+    if (!req.header('Authorization')) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
     try {
+        const token = req.header('Authorization').split(' ')[1];
         const decoded = jwt.verify(token, config.secret_jwt);
         req.user = decoded.user;
         next();
