c52e31d4a46198a04965f4560830b6e381870ef7
Dimitri Perriet <dimitri.perriet@outlook.fr>
2023-06-21 09:10:14 +0200

    UPDATE : Database

--- openai.sql
+++ openai.sql
@@ -24,18 +24,6 @@ CREATE DATABASE IF NOT EXISTS `openai` DEFAULT CHARACTER SET utf8 COLLATE utf8_g
 USE `openai`;
 
 -- --------------------------------------------------------
-
---
--- Doublure de structure pour la vue `all_users`
--- (Voir ci-dessous la vue réelle)
---
-CREATE TABLE `all_users` (
-                             `ID` int(11)
-    ,`mail` varchar(255)
-    ,`lastname` varchar(255)
-    ,`firstname` varchar(255)
-);
-
 -- --------------------------------------------------------
 
 --
@@ -97,15 +85,6 @@ CREATE TABLE `game_character` (
 
 -- --------------------------------------------------------
 
---
--- Doublure de structure pour la vue `game_characters`
--- (Voir ci-dessous la vue réelle)
---
-CREATE TABLE `game_characters` (
-                                   `game_name` varchar(255)
-    ,`character_name` varchar(255)
-);
-
 -- --------------------------------------------------------
 
 --
@@ -123,22 +102,6 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
-
---
--- Structure de la vue `all_users`
---
-DROP TABLE IF EXISTS `all_users`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `all_users`  AS SELECT `user`.`ID` AS `ID`, `user`.`mail` AS `mail`, `user`.`lastname` AS `lastname`, `user`.`firstname` AS `firstname` FROM `user``user`  ;
-
--- --------------------------------------------------------
-
---
--- Structure de la vue `game_characters`
---
-DROP TABLE IF EXISTS `game_characters`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `game_characters`  AS SELECT `games`.`name` AS `game_name`, `game_character`.`name` AS `character_name` FROM (`games` join `game_character` on((`games`.`ID` = `game_character`.`game_id`)))  ;
 
 --
 -- Index pour les tables déchargées
